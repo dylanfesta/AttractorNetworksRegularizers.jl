dc90ed5745388d92d64d2dd9d6f434be9b92e0c5
Dylan <darksnail@gmail.com>
2019-03-23 13:15:26 -0400

    First new version, time for testing and debugging!

--- src/AttractorNetworksRegularizers.jl
+++ src/AttractorNetworksRegularizers.jl
@@ -109,7 +109,7 @@ however there can be "regional" assignments smaller in size
 that can be used for "regional" operations over the elements.
 
 A function ensures that the global indices of the regional assignment still point
-at the correct positions of the global vector. 
+at the correct positions of the global vector.
 =#
 
 # appends glo and loc to the symbol name
@@ -121,6 +121,12 @@ function _gloloc(v::AbstractVector{Symbol})
   end
   glo,loc
 end
+
+key_isglo(k) = occursin(r"\_glo\b",String(k))
+key_isloc(k) = occursin(r"\_loc\b",String(k))
+key_nosuffix(k) = Symbol(String(k)[1:end-4])
+key_toglobal(k) = Symbol(key_nosuffix(k),:_glo)
+key_tolocal(k) = Symbol(key_nosuffix(k),:_loc)
 
 # linear indexes of nonmissing values
 function get_nonmissing(bu::AbstractVector)::Vector{Int64}
@@ -143,14 +149,15 @@ function make_assignment(elements,selector)
   names_loc,names_glo = _gloloc(keys(selector))
   idx_loc = map(get_nonmissing,values(selector))
   # global: one index for each local element
-  idx_glo = let  lgs = length.(idx_loc)
-    whole = collect(1:sum(lgs))
+  lgs = length.(idx_loc)
+  nglo = sum(lgs)
+  idx_glo = let whole = collect(1:sum(lgs))
     [splice!(whole,1:l) for l in lgs]
   end
   names_all = vcat(names_loc,names_glo)
   idx_all =vcat(idx_loc, idx_glo)
   # put them together in a named tuple
-  NamedTuple{Tuple(names_all)}(idx_all)
+  (nglo, NamedTuple{Tuple(names_all)}(idx_all) )
 end
 
 """
@@ -163,8 +170,7 @@ capital assignment
 """
 function globalize(assignent_reg, assignment_cap)
   # extract the relevant names of keys
-  isloc(s) = occursin(r"\_loc\b",String(s))
-  keys_loc = filter(isloc, keys(assignent_reg))
+  keys_loc = filter(key_isloc, keys(assignent_reg))
   keys_glo = map(keys_loc) do k
     Symbol(String(k)[1:end-4]*"_glo")
   end
@@ -183,18 +189,44 @@ function globalize(assignent_reg, assignment_cap)
   end
 end
 
+"""
+      packing!(v::AbstractVector,elements::NamedTuple,
+            assignment::NamedTuple , forward_mode::Bool=true)
+
+Using the indexes in `assignment` , picks the relevant values in `elements` and
+either packs them in the `v` vector `forward_mode=true` or from `v` writes them
+back into `elements`  `forward_mode=false`
+"""
+function packing!(v::AbstractVector,elements::NamedTuple,
+      assignment::NamedTuple , forward_mode::Bool=true)
+  isglo(s) = occursin(r"\_glo\b",String(s))
+  for (k_glo,idxs) in pairs(assignment)
+    if key_isglo(k_glo) && (!isempty(idxs))
+      k_el = key_nosuffix(k)
+      k_loc = Symbol(k_el,:_loc)
+      idx_glo = getfield(assignment,k_glo)
+      idx_loc =  getfield(assignment,k_loc)
+      if forward_mode
+        view(v,idx_glo) .= view(getfield(elements,k_el), idx_loc)
+      else
+        view(getfield(elements,k_el), idx_loc) .= view(v,idx_glo)
+      end
+    end
+  end
+  nothing
+end
+function unpack!(v,els,as) = packing!(v,els,as,false)
 
 
 struct RegularizerPack
   elements::NamedTuple # the elements that are packed, all arrays
+  assignments::NamedTuple  # Global assignement that regulates the poisitions in x
   allocs::NamedTuple  # space used for gradients and stuff, same size as elements
-  x::Vector{Float64}  # variables after regularization
-  xnnonreg::Vector{Float64} # variables before regularization
   gradx::Vector{Float64}  # gradient of regularizer w.r.t. xnonreg
+  xreg::Vector{Float64}  # variables in regu form
+  xnnonreg::Vector{Float64} # variables in unbound form
   regus::Vector{Regularizer}  # regularizes, element by element
-  indexes::NamedTuple  # all the indexing goes here
 end
-
 
 # takes a named tuple of arrays
 # returns tuple with same keys, but where the arrays are
@@ -206,7 +238,6 @@ function make_empty_selection(elements)
   end
   NamedTuple{keys(elements)}( map(makeone,values(elements)) )
 end
-
 
 
 """
@@ -242,290 +273,66 @@ function RegularizerPack(elements, selections, regudefs)
   dups = [ similar(x) for x in  values(elements)]
   allocs = NamedTuple{keys(elements)}(dups)
   # now, the indexes tuple...
-  names_loc,names_glo = _gloloc(keys(selections))
-  # names_loc = [ _gloloc(k)[2] Symbol(k,"_loc") for k in keys(selections)]
-  # names_glo = [ Symbol(k,"_glo") for k in keys(selections)]
-  idx_loc = map(_linear_nonmissing,values(selections))
-  # and the global ones, too!
-  counter=0
-  idx_glo = map(idx_loc) do idx
-    o = collect(1:length(idx)) .+ counter
-    counter += length(o)
-    o
-  end
-  names_all = vcat(names_loc,names_glo)
-  idx_all =vcat(idx_loc, idx_glo)
-  # put them together in a named tuple
-  indexes =  NamedTuple{Tuple(names_all)}(idx_all)
+  (nglobs, assignments) = make_assignment(element,selections)
   # now I need to assign the regularizers
   # this means packing!
-  regus = Vector{Regularizer}(undef,counter)
-  _regus_aux = Vector{Symbol}(undef,counter)
-  for (nm,vv) in pairs(selections)
-    nm_glo,nm_loc = _gloloc(nm)
-    idx_glo = getfield(indexes,nm_glo)
-    idx_loc =  getfield(indexes,nm_loc)
-    regusloc = vv[idx_loc]
-    _regus_aux[idx_glo] .= regusloc
-  end
+  _regus_aux = Vector{Symbol}(undef,nglobs)
+  packing!(_regus_aux , selections, assignments)
   for regunm in unique(_regus_aux)
     @assert regunm in keys(regudefs)
   end
+  regus = Vector{Regularizer}(undef,nglobs)
   for (i,rr) in enumerate(_regus_aux)
     regus[i] = getfield(regudefs,rr)
   end
-  x,gradx = [ Vector{Float64}(undef,counter) for _ in 1:2 ]
+  xreg,xnonreg,radx = [ Vector{Float64}(undef,nglobs) for _ in 1:3 ]
+  #define it
+  rpack=RegularizerPack(elements,assignments,allocs,xreg,xnonreg,gradx,regus)
+  # initialize it !
 
-  # all done!
-  RegularizerPack(elements,allocs,x,gradx, regus,indexes)
 end
 Base.length(p::RegularizerPack) = length(p.x)
 Base.keys(p::RegularizerPack) = keys(p.elements)
 
-# vectorized versions!
-for fname in (:reg,:dreg,:ireg)
-  fname! = Symbol(fname,"!")
-  eval( :(
-    function $fname!(pk::RegularizerPack)
-      @. pk.x = $fname(pk.xnnonreg,pk.regus)
-    end))
-end
+##
 
-function _load_pack!(newelements,pk::RegularizerPack)
-  for (k,vv) in pairs(newelements)
-    k_glo,k_loc = _gloloc(k)
-    idx_loc = getfield(pk.indexes,k_loc)
-    idx_glo = getfield(pk.indexes,k_glo)
-    pk.xnnonreg[idx_glo] .= vv[idx_loc]
-  end
+function pack_allx!(xreg,xnonreg,elements,assignments,regus)
+  # first extract the variables from elements, place them in the buffer
+  # elements are already regularized!
+  packing!(xreg,elements,assignments)
+  # now convert!
+  @. xnonreg =  ireg(xreg,regus)
   nothing
 end
-function _load_pack!(pk::RegularizerPack) = _load_pack(pk.elements,pk)
-
-"""
-Auxiliary function that does the opposite of loading x
-that is: takes the contents of x and writes them in the
-unpacked named tuple.
-It basically transfer the value in the opposite direction
-"""
-function _unload_pack!(newx, pk::RegularizerPack)
-  _x = newx
-  for (k,arr) in pairs(pk.elements)
-    k_glo,k_loc = _gloloc(k)
-    idx_loc = getfield(pk.indexes,k_loc)
-    idx_glo = getfield(pk.indexes,k_glo)
-    # exact same of load, except I invert this!
-    vv[idx_loc] .= _x[idx_glo]
-  end
-  nothing
+function pack_allx!(rp::RegularizerPack)
+  pack_allx(rp.xreg,rp.xnonreg,rp.elements,rp.assignments,rp.regus)
+end
+function pack_calc_grad!(xgrad, xnonreg, regus)
+  @. xgrad = dreg(xnonreg,regus)
 end
 
-#=
-"""
-Converts the elements of x using the regularizers specified by the tuple
-at the indexes specified by the other tuple
-"""
-function reg!(y,regu_defs,regu_targ,x)
-for (k,idx) in pairs(regu_targ)
-regu=regu_defs[k]
-reg!(view(y,idx),view(x,idx),regu)
+function pack_allx_grad!(xreg,xnonreg,xgrad,elements,assignments,regus)
+  pack_allx!(xreg,xnonreg,elements,assignments,regus)
+  pack_calc_grad!(xgrad, xnonreg, regus)
 end
-y
+function pack_allx_grad!(rp::RegularizerPack)
+   pack_allx_grad!(rp.xreg,rp.xnonreg,rp.xgrad,rp.elements,rp.assignments,rp.regus)
 end
-function dreg!(y,regu_defs,regu_targ,x)
-for (k,idx) in pairs(regu_targ)
-regu=regu_defs[k]
-dreg!(view(y,idx),view(x,idx),regu)
-end
-y
-end
-function reginv!(y,regu_defs,regu_targ,x)
-for (k,idx) in pairs(regu_targ)
-regu=regu_defs[k]
-reginv!(view(y,idx),view(x,idx),regu)
-end
-y
-end
-
-
-"""
-Auxiliary function: reads the internal unpacked tuple, or the one
-specified, and fills the interal y_alloc vector with the right order
-"""
-function _load_y(y,rp::RegularizerPack,unpacked)
-for (k,arr) in pairs(unpacked)
-idx_loc = rp.indexes_local[k]
-idx_pack = rp.indexes_packed[k]
-y[idx_pack] = arr[idx_loc]
-end
-nothing
-end
-function _load_y(rp::RegularizerPack,unpacked)
-y = rp.y_alloc
-_load_y(y, rp, unpacked)
-end
-function _load_y(rp::RegularizerPack)
-y = rp.y_alloc
-_load_y(y,rp,rp.unpacked)
-nothing
-end
-function _load_y(y::AbstractVector,
-rp::RegularizerPack,unp,symbs::AbstractVector{S}) where S<: Symbol
-for k in symbs
-arr=unp[k]
-idx_loc = rp.indexes_local[k]
-idx_pack = rp.indexes_packed[k]
-y[idx_pack] = arr[idx_loc]
-end
-nothing
-end
-
-"""
-Auxiliary function that does the opposite of loading y
-that is: takes the contents of y and writes them in a dictionary
-It basically transfer the value in the opposite direction
-"""
-function _unload_y(rp::RegularizerPack,unpacked)
-y = rp.y_alloc
-for (k,arr) in pairs(unpacked)
-idx_loc = rp.indexes_local[k]
-idx_pack = rp.indexes_packed[k]
-arr[idx_loc] = y[idx_pack]   # literally the same of load, except I invert this!
-end
-nothing
-end
-function _unload_y(rp::RegularizerPack)
-_unload_y(rp,rp.unpacked)
-nothing
-end
-"""
-Needed for initialization: reads the internal dictionary,
-uses it to produce a parameter vector x
-"""
-function pack(rp::RegularizerPack)
-_load_y(rp)
-reginv!(rp.x_alloc,rp.regu_defs,rp.regu_targets ,rp.y_alloc)
-end
-
-"""
-After a computation of the gradients into the
-arrays in g_unpacked, call this function to  pack the elements
-of the unpacked arrays in the vector specified
-"""
-function g_pack!(g::AbstractArray{T,1},rp::RegularizerPack) where T<:Real
-_load_y(g,rp,rp.g_unpacked)
-end
-function g_pack!(g::AbstractArray{T,1},
-rp::RegularizerPack,symbs::AbstractVector{S}) where {T<:Real,S<:Symbol}
-_load_y(g,rp,rp.g_unpacked,symbs)
-end
-
-"""
-Inverts the contents of the vector x and
-writes them  in the unpacked dictionary
-elements of the packaging
-"""
-function unpack(x::AbstractVector,rp::RegularizerPack)
-copyto!(rp.x_alloc,x)
-reg!(rp.y_alloc,rp.regu_defs,rp.regu_targets , x)
-_unload_y(rp)
-nothing
-end
-
-function unpack_grad(x::AbstractVector,rp::RegularizerPack)
-unpack(x,rp) # this updates x_alloc and y_alloc
-dreg!(rp.g_alloc, rp.regu_defs,rp.regu_targets,rp.x_alloc) # this returns g_alloc
-end
-
-"""
-Constructors that also fills the packed part, and
-returns the inital x vector along with the packed object
-"""
-function RegularizerPack_init(regu_defs, regu_attrib, unpacked)
-rp=RegularizerPack(regu_defs,regu_attrib,unpacked)
-x0 = pack(rp)  # careful, this is also x_alloc... maybe I should make a copy?
-(rp,x0)
-end
-
-
-
-# Pack that contains the parameters (arrays of floats)
-# and the regularizers to use on each.
-# It pushes everything into a long vector that can be used for optimization
-struct RegularizerPack{Tf<:NamedTuple, Ti<:NamedTuple,
-        Ta<:NamedTuple, Tt<:NamedTuple, V<:AbstractVector}
-    regu_defs::Ta # symbol to regularizer
-    unpacked::Tf  # network components in their natural shape
-    indexes_local::Ti # which of the above should be packed
-    indexes_packed::Ti # where they go in the packed vector
-    g_unpacked::Tf  # space allocated for gradients
-    regu_targets::Tt # used to link local indexes with regularizer type
-    x_alloc::V  # allocation for finv, or for argument
-    y_alloc::V  # allocation for f
-    g_alloc::V # allocation for df
-end
-function RegularizerPack(regu_def,
-    regu_attrib,
-    unpacked) where Regu<:Regularizer
-
-    # create the allocations for the gradient
-    g_unpacked = NamedTuple{ keys(regu_attrib)}([ zero(unpacked[k])
-                        for k in keys(regu_attrib) ])
-    indexes_local=Dict{Symbol,Vector{Int64}}()
-    indexes_packed=Dict{Symbol,Vector{Int64}}()
-    _idx_loc = Vector{Int64}[]
-    _idx_pac = Vector{Int64}[]
-    all_symb = Symbol[]
-    pack_size = 0
-    for (key,rmat) in pairs(regu_attrib)
-        idxs = let
-            lin = LinearIndices(rmat)
-            lin[.!ismissing.(rmat)]
-        end
-        push!(_idx_loc,idxs)
-        n_els = length(idxs)
-        push!(_idx_pac, collect(1:n_els) .+ pack_size)
-        pack_size += n_els
-        append!(all_symb , convert(Vector{Symbol}, rmat[idxs]))
-    end
-    indexes_local = NamedTuple{keys(regu_attrib)}(_idx_loc)
-    indexes_packed = NamedTuple{keys(regu_attrib)}(_idx_pac)
-    @assert length(all_symb) == pack_size
-    x_alloc,y_alloc,g_alloc = (Vector{Float64}(undef,pack_size) for i in 1:3)
-
-    regu_targets = let  _usymb = unique(all_symb)
-        NamedTuple{Tuple(_usymb)}( [ findall(all_symb .==s) for s in _usymb ])
-    end
-    RegularizerPack(regu_def,unpacked,indexes_local,
-        indexes_packed,g_unpacked, regu_targets, x_alloc,y_alloc,g_alloc)
-end
-
-
-"""
-Makes a tuple with same entries as val_dict,
-that can be used to initialize a RegularizerPack object
-"""
-function make_regus_tuple(val_tuple)
-    NamedTuple{keys(val_tuple)}(
-     [ fill!(similar(arr,Union{Missing,Symbol}),missing) for arr in values(val_tuple)] )
-end
-
 
 function gradient_test(rp::RegularizerPack,x::AbstractVector)
-    @assert length(rp) == length(x)
-    g_an = unpack_grad(x,rp) # careful, this is g_alloc
+    fill!(pr.xnonreg ,x)
+    g_an = copy(pack_calc_grad(rp.xgrad,pr.xnonreg,rp.regus)) # careful, this is g_alloc
     eps_test=1E-8
     # all grads are independent, so I can vectorize
     xpm=copy(x)
     xpm .+= eps_test
-    f_p=similar(xpm)
-    reg!(f_p, rp.regu_defs,rp.regu_targets, xpm)
+    f_p = map(reg, zip(xpm,rp.regus) )
     xpm .-= 2eps_test
-    f_m=similar(xpm)
-    reg!(f_m,rp.regu_defs,rp.regu_targets, xpm)
+    f_m = map(reg, zip(xpm,rp.regus) )
     g_num = @. (f_p-f_m) / (2*eps_test)
     diff_g = @. abs( 2. * (g_num-g_an)/(g_num+g_an) )
     g_num , g_an , diff_g
 end
-=#
+
+
 end # module
